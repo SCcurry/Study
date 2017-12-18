a035db1127a9d04af5112eba1255c274d0dc6777
curry <1812584700@qq.com>
2017-12-19 00:05:05 +0800

    常用控件的使用，并且获取了用户信息

--- app/src/main/java/com/example/administrator/study/MainActivity.java
+++ app/src/main/java/com/example/administrator/study/MainActivity.java
@@ -4,8 +4,10 @@ package com.example.administrator.study;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
@@ -14,7 +16,14 @@ public class MainActivity extends AppCompatActivity
     private Button btn_save;
     private Button btn_clear;
     private Button btn_jump;
+    private Button btn_bigger;
+    private Button btn_smaller;
     private TextView tv;
+    private EditText u_name;
+    private EditText u_psaaward;
+    private EditText u_email;
+    private int tv_Size=20;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -24,18 +33,63 @@ public class MainActivity extends AppCompatActivity
 
         btn_save=(Button)findViewById(R.id.save);
         btn_clear=(Button)findViewById(R.id.clear);
-        btn_jump=(Button)findViewById(R.id.jump);
+
+        btn_bigger=(Button)findViewById(R.id.bigger);
+        btn_smaller=(Button)findViewById(R.id.smaller);
 
         tv=(TextView)findViewById(R.id.tv);
+
+        u_name=(EditText)findViewById(R.id.user_name);
+        u_email=(EditText)findViewById(R.id.user_email);
+        u_psaaward=(EditText)findViewById(R.id.user_passward);
+
+
+
+
         //给当前btn做监听
         btn_save.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                tv.setText("保存成功！");
+
+                Intent i=new Intent(MainActivity.this,NewActivity.class);
+
+                String name_name=u_name.getText().toString();
+                String name_passward=u_psaaward.getText().toString();
+                String name_email=u_email.getText().toString();
+
+                String user_infor="用户的用户名，密码，邮箱分别为："+name_name+","+name_passward+","+name_email;
+                i.putExtra("name",user_infor);
+                startActivity(i);
+            }
+
+
+
+        });
+
+        btn_smaller.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(tv_Size>20) {
+                    tv.setTextSize(--tv_Size);
+                }
             }
         });
+
+        btn_bigger.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(tv_Size<30) {
+                    tv.setTextSize(++tv_Size);
+                }
+            }
+        });
+
         btn_clear.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -44,18 +98,6 @@ public class MainActivity extends AppCompatActivity
                 tv.setText("清除成功！");
             }
         });
-        btn_jump.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                //intent必须实例化
-                Intent intent =new Intent(MainActivity.this,NewActivity.class);
-                //传值准备
-                intent.putExtra("name","第一次intent的实现,如果这个页面出来了，说明intent实现跳转传递数据你已经会了");
-                //开始跳转
-                startActivity(intent);
-            }
-        });
+
     }
 }
